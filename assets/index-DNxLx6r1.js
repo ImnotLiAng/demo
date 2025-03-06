5db22d8ea64bebcec8116b7edfc0c4e7bdff0cb1
lizibin <lizibin>
2025-03-06 20:38:53 +0800

    update

---
+++ assets/index-DNxLx6r1.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CVqT7AEs.js";function n(){return r.jsx(r.Fragment,{children:r.jsx("div",{children:"conversation"})})}export{n as default};
--- assets/index-XR--7juQ.js
+++
@@ -1,1 +0,0 @@
-import{u as e,j as s}from"./index-LiiJobG9.js";const a=()=>{const{id:r}=e();return s.jsx(s.Fragment,{children:s.jsxs("div",{children:["conversation: ",r]})})};export{a as default};
